d265eec478a656724c1c20752c69dc00c5bfca7d
Dan Schissler <schisslerdg@gmail.com>
2019-10-19 21:14:54 -0700

    working draft - no css

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,5 @@
 var wordBank = [
     "poseidon",
-    "hades",
     "zeus",
     "hestia",
     "hera",
@@ -11,46 +10,167 @@ var wordBank = [
     "apollo",
     "aphrodite",
     "hermes",
-    "athena",
-    "dionysus"
+    "athena"
 ]
 var wordArray = [];
 var blanksArray = [];
+var incorrectArray = [" "];
 var currentWord = "";
 var userGuess = "";
+let lettersRemaining = 0;
+var chancesLeft = 8;
+let numberOfLetters = 0;
+var letterPresent = false;
 
 
-//Grabs a word from possible choices
-var currentWord = wordBank[Math.floor(Math.random() * wordBank.length)];
+//Begins a new game for user
+function beginNewGame() {
+    //Grabs a word from possible choices
+    var currentWord = wordBank[Math.floor(Math.random() * wordBank.length)];
+    numberOfLetters = currentWord.length;
+    lettersRemaining += numberOfLetters;
+    
+    //creates new array from currentWord to verify guesses by index
+    for (var j = 0; j < numberOfLetters; j++) {
+        wordArray.push(currentWord[j]);
+    }
+
+    //Hides the word behind blanks
+    for (var i = 0; i < numberOfLetters; i ++) {
+        blanksArray.push("_");
+    }
+
+    //error checking
+    console.log(currentWord);
+    console.log(blanksArray);
+    console.log(wordArray);
+
+    document.getElementById("unkown-olympian").innerHTML = blanksArray.join(" ");
+};
 
 
-//Logs the word user is trying to guess
-console.log(currentWord);
+//Outcomes with image and bio
+function reward() {
+    switch (wordArray.join("")) {
+        case "poseidon":
+            document.getElementById("ending-image").src="./assets/images/poseidon.jpg"
+            break;
 
-//Transforms string into array
-for (var i = 0; i < currentWord.length; i++) {
-    wordArray.push(currentWord[i])
-}
-console.log(wordArray)
-console.log(currentWord)
+        case "zeus":
+                document.getElementById("ending-image").src="./assets/images/zeus.jpg"
+            break;
 
-//Takes the new word and replaces each letter as a blank on a new array
-function hiddenWord() {
-    for (var j = 0; j < currentWord.length; j++) {
-      blanksArray.push("_");   
+        case "hestia":
+                document.getElementById("ending-image").src="./assets/images/hestia.jpg"
+            break;
+
+        case "hera":
+                document.getElementById("ending-image").src="./assets/images/hera.jpg"
+            break;
+
+        case "demeter":
+                document.getElementById("ending-image").src="./assets/images/demeter.jpg"
+            break;
+
+        case "ares":
+                document.getElementById("ending-image").src="./assets/images/ares.jpg"
+            break;
+
+        case "hephaestus":
+                document.getElementById("ending-image").src="./assets/images/hephaestus.jpg"
+            break;
+
+        case "artemis":
+                document.getElementById("ending-image").src="./assets/images/artemis.jpg"
+            break;
+
+        case "apollo":
+                document.getElementById("ending-image").src="./assets/images/apollo.jpg"
+            break;
+
+        case "aphrodite":
+                document.getElementById("ending-image").src="./assets/images/aphrodite.jpg"
+            break;
+
+        case "hermes":
+                document.getElementById("ending-image").src="./assets/images/hermes.jpg"
+            break;
+
+        case "athena":
+                document.getElementById("ending-image").src="./assets/images/athena.jpg"
+            break;
+
+        default:
+            break;
+
     }
 }
 
-//Logs the word with a blank for each letter
-console.log(blanksArray);
+
+//Game play of guessing and checking the guess against the random word
+function checkGuess(userGuess) {
+    for (var j = 0; j < numberOfLetters; j++) {
+        if (userGuess === wordArray[j]) {
+            blanksArray[j] = userGuess;
+            letterPresent = true;
+
+        } else {
+            letterPresent = false;
+        }  
+    }
+
+    if (letterPresent === false) {
+        incorrectArray.push(userGuess); 
+        chancesLeft--;
+
+    } else {
+                lettersRemaining--;
+
+        }
 
 
+            
+   
 
-//Takes userGuess and searches array for match
-function searchArray() {
-    for (k = 0; k < currentWord.length; k++) {
-        if (userGuess === wordArray[k]) {
-            blanksArray.push(userGuess[k];
-        } 
+
+    document.getElementById("attempts").innerHTML = "You have " + chancesLeft + " guesses before you lose.";
+    document.getElementById("passedGuesses").innerHTML = incorrectArray.join(" ");
+    document.getElementById("unkown-olympian").innerHTML = blanksArray.join(" ");
+    console.log("letters remaining in word " + lettersRemaining);
+    console.log("number of guesses remaining " + chancesLeft);
+    
+}
+
+//Ending the Game
+
+function endOfGame() {
+    if (lettersRemaining === 0) {
+        reward()
+        document.getElementById("result").innerHTML = "You Won!"
+    } else if (chancesLeft === 0) {
+        document.getElementById("ending-image").src = "./assets/images/loser.jpg"
+        document.getElementById("result").innerHTML = "Sorry you lose!"
+    } else {
+        alert("you broke the game!")
     }
 }
+
+
+
+
+   
+
+
+
+window.onload = function() {
+    beginNewGame();
+}
+
+document.onkeyup = function (event) {
+    var userGuess = event.key.toLowerCase();
+    checkGuess(userGuess);
+    if (lettersRemaining === 0 || chancesLeft === 0) {
+        endOfGame();
+    }   
+}   
+
--- assets/javascript/working-game.js
+++ assets/javascript/working-game.js
@@ -1,6 +1,5 @@
 var wordBank = [
     "poseidon",
-    "hades",
     "zeus",
     "hestia",
     "hera",
@@ -11,70 +10,182 @@ var wordBank = [
     "apollo",
     "aphrodite",
     "hermes",
-    "athena",
-    "dionysus"
+    "athena"
 ]
 var wordArray = [];
 var blanksArray = [];
 var incorrectArray = [];
 var currentWord = "";
 var userGuess = "";
-var letterleft = currentWord.length;;
+var lettersRemaining = 0;
 var chancesLeft = 10;
+var numberOfLetters = 0;
 
 
-//When user selects new game word is chosen at random and arrays are made to show number of letters and to check guesses correct or incorrect
+//Begins a new game for user
 function beginNewGame() {
     //Grabs a word from possible choices
     var currentWord = wordBank[Math.floor(Math.random() * wordBank.length)];
-
-
-    //Logs the word user is trying to guess
-    console.log(currentWord);
+    numberOfLetters = currentWord.length;
+    lettersRemaining += numberOfLetters;
+    
+    //creates new array from currentWord to verify guesses by index
+    for (var j = 0; j < numberOfLetters; j++) {
+        wordArray.push(currentWord[j]);
+    }
 
     //Hides the word behind blanks
-    for (var i = 0; i < currentWord.length; i ++) {
-    blanksArray.push("_");
+    for (var i = 0; i < numberOfLetters; i ++) {
+        blanksArray.push("_");
     }
 
-    //Logs the new blanksArray
+    //error checking
+    console.log(currentWord);
     console.log(blanksArray);
-
-    //creates new array from currentWord to verify guesses by index
-    for (var j = 0; j < currentWord.length; j++) {
-    wordArray.push(currentWord[j]);
-    }
-
-    //logs the wordArray
     console.log(wordArray);
 
-    //shows blanks for current word to user
-    document.getElementById("unkownOlympian").innerHTML = blanksArray.join(" ");
+    document.getElementById("unkown-olympian").innerHTML = blanksArray.join(" ");
 };
 
 
-//verify if guess is correct
-for (var k = 0; k < currentWord.length; k++) {
-    if (userGuess === wordArray[k]) {
-        blankArray[k] = userGuess;
-        
-    } else {
-        incorrectArray.push(userGuess);``
-        chancesLeft--;
-        console.log(chancesLeft);
+//Game play of guessing and checking the guess against the random word
+function checkGuess(userGuess) {
+    for (var k = 0; k < incorrectArray.length; k++) {
+        if (userGuess === incorrectArray[k]){
+            alert("You already guessed " + userGuess + "!")
+        }
+    } //else
+
+    for (var j = 0; j < numberOfLetters; j++) {
+        if (userGuess === wordArray[j]) {
+            var letterPresent = true;
+        } 
     }
+
+//conditional if not checked then coutdown
+
+    if (letterPresent === true) {
+        for (var i = 0; i < numberOfLetters; i++) {
+            if (userGuess === wordArray[i]) {
+                console.log("true")
+                blanksArray[i] = userGuess;
+                lettersRemaining--;
+            }
+        } 
+    } else {
+        incorrectArray.push(userGuess);
+        chancesLeft--;
+        console.log("number of guesses remaining " + chancesLeft);
+    }
+    
+    
+    document.getElementById("attempts").innerHTML = "You have " + chancesLeft + " guesses before you lose.";
+    document.getElementById("passedGuesses").innerHTML = incorrectArray.join(" ");
+    document.getElementById("unkown-olympian").innerHTML = blanksArray.join(" ");
+    console.log("letters remaining in word " + lettersRemaining);
+}
+
+//Outcomes with image and bio
+function reward() {
+    switch (wordArray.join("")) {
+        case "poseidon":
+            document.getElementById("ending-image").src="./assets/images/poseidon.jpg"
+            break;
+
+        case "zeus":
+                document.getElementById("ending-image").src="./assets/images/zeus.jpg"
+            break;
+
+        case "hestia":
+                document.getElementById("ending-image").src="./assets/images/hestia.jpg"
+            break;
+
+        case "hera":
+                document.getElementById("ending-image").src="./assets/images/hera.jpg"
+            break;
+
+        case "demeter":
+                document.getElementById("ending-image").src="./assets/images/demeter.jpg"
+            break;
+
+        case "ares":
+                document.getElementById("ending-image").src="./assets/images/ares.jpg"
+            break;
+
+        case "hephaestus":
+                document.getElementById("ending-image").src="./assets/images/hephaestus.jpg"
+            break;
+
+        case "artemis":
+                document.getElementById("ending-image").src="./assets/images/artemis.jpg"
+            break;
+
+        case "apollo":
+                document.getElementById("ending-image").src="./assets/images/apollo.jpg"
+            break;
+
+        case "aphrodite":
+                document.getElementById("ending-image").src="./assets/images/aphrodite.jpg"
+            break;
+
+        case "hermes":
+                document.getElementById("ending-image").src="./assets/images/hermes.jpg"
+            break;
+
+        case "athena":
+                document.getElementById("ending-image").src="./assets/images/athena.jpg"
+            break;
+
+        default:
+            break;
+
+    }
+}
+
+//Ending the Game
+
+function endOfGame() {
+    if (lettersRemaining === 0) {
+        alert("you won!")
+    } else if (chancesLeft ===0) {
+        alert("you lost!")
+    } else {
+        alert("you broke the game!")
+    }
+}
+
+//Outcomes with image and bio
+
+
+   
+
 
 
 window.onload = function() {
     beginNewGame();
-
 }
-//Game loop
+
+document.onkeyup = function (event) {
+    var userGuess = event.key.toLowerCase();
+    checkGuess(userGuess);
+    if (lettersRemaining === 0 || chancesLeft === 0) {
+        endOfGame();
+    }   
+}   
 
 
-// document.onkeyup = function(event) {
-//     var userGuess = event.key.toLowerCase();
-//     
-//     }
-    
-// }
+
+"zeus",
+
+"hera",
+"hestia",
+"demeter",
+"hermes",
+"hephaestus",
+"apollo",
+"artemis",
+"athena",
+"aphrodite",
+"ares"
+
+
